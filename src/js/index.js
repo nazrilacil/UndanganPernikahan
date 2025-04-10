7ce9c4400262c1fbb11c176d41ea7d895d59398d
Nazril Acil <nazrilacil04@gmail.com>
2025-04-11 02:16:21 +0700

    Update index.js

--- src/js/index.js
+++ src/js/index.js
@@ -13,3 +13,35 @@ document.addEventListener('DOMContentLoaded', function() {
             seconds: { root: 'detik', lambda: (root, n) => n > 1 ? root + '' : root }}
     });
 });
+
+const offCanvas = document.querySelector('.offcanvas');
+const stickyTop = document.querySelector('.sticky-top');
+
+offCanvas.addEventListener('show.bs.offcanvas', function() {
+    stickyTop.style.overflow = 'visible';
+});
+
+offCanvas.addEventListener('hidden.bs.offcanvas', function() {
+    stickyTop.style.overflow = 'hidden';
+});
+
+const rootElement = document.querySelector(':root');
+
+function disableScroll() {
+    scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+    scrollLeft = window.pageXOffset || document.documentElement.scrollLeft;
+
+	window.onscroll = function() {
+		window.scrollTo(scrollTop, scrollLeft);
+	}
+	rootElement.style.scrollBehavior = 'auto';
+}
+
+function enableScroll() {
+	window.onscroll = function() { }
+	rootElement.style.scrollBehavior = 'smooth';
+    localStorage.setItem('opened', 'true');
+}
+if(!localStorage.getItem('opened')) {
+    disableScroll();
+}
